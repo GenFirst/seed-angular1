21f8594c09549f20d4ec5f258f3140b6c372dae4
vdimitrieski <dimitrieski89@gmail.com>
2016-07-23 15:04:04 +0200

    basic gulp tasks (watch and lint)

--- my-project/gulpfile.js
+++ my-project/gulpfile.js
@@ -1,5 +1,24 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-    // place code for your default task here
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+//test the quality of the code with ESLint
+gulp.task('lint', function () {
+    return gulp.src(['app/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.formatEach())
+        .pipe(eslint.failAfterError());
 });
+
+//watch changes in the app code (html and js files)
+gulp.task('watch', function () {
+    gulp.watch([
+        './app/**/*.js',
+        './app/**/*.html'
+    ], ['lint']);
+});
+
+//default task specification
+gulp.task('default', ['watch']);
+
